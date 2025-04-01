c233d392d643617cdb8696e8acf32c59ca311c7b
ladev325 <vlad2008artemov@gmail.com>
2025-04-01 16:58:16 +0300

    CW

--- Example_1/main.cpp
+++ Example_1/main.cpp
@@ -1,23 +1,7 @@
 ﻿#include <iostream>
-#include <fstream>
-#include "Adapter.h"
 using namespace std;
 
-int Adapter::counter = 0;
-
 int main() {
-	ofstream file("file.txt");
-	if (!file) {
-        cout << "Error opening" << endl;
-        return 1;
-    }
-
-	Adapter ad1("USB0-A", 200);
-	Adapter ad2("USB-C", 400);
-	Adapter ad3("Micro-USB", 250);
-
-	file << ad1 << ad2 << ad3;
-
-	file.close();
+	
 	return 0;
 }
